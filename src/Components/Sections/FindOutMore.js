7e3bac28788aa4fa90ac65b6bf7692a71e037676
Luke Charles <lukeowaincharles@gmail.com>
2020-03-30 21:59:04 +0100

    Added icons as ReactComponents, svgs

--- src/Components/Sections/FindOutMore.js
+++ src/Components/Sections/FindOutMore.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ButtonPrimary from '../Buttons/ButtonPrimary';
-import FindOutMoreData from "../../Data/FindOutMoreData.json"
+import FindOutMoreData from "../../Data/FindOutMoreData.json";
+import { ReactComponent as CodeIcon } from "../../Assets/images/coding.svg";
+import { ReactComponent as HashIcon } from "../../Assets/images/hashtag.svg";
+import { ReactComponent as HeartIcon } from "../../Assets/images/heart.svg";
 
 function FindOutMore() {
   const CONTENT = FindOutMoreData.content;
@@ -14,6 +17,9 @@ function FindOutMore() {
           title={"Find out more"}
         />
       </div>
+      <CodeIcon className="icon icon--code" />
+      <HashIcon className="icon icon--hash" />
+      <HeartIcon className="icon icon--heart" />
     </section>
   )
 }
