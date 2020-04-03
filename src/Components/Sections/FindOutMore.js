a3cb92258a72f4ee78fc73c22db99613b9ddc67d
Luke Charles <lukeowaincharles@gmail.com>
2020-04-03 22:07:30 +0100

    Added hooks for setTimeout animation for content to fadeIn

--- src/Components/Sections/FindOutMore.js
+++ src/Components/Sections/FindOutMore.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import ButtonPrimary from '../Buttons/ButtonPrimary';
+import React, { useState, useEffect } from "react";
+import ButtonPrimary from "../Buttons/ButtonPrimary";
 import FindOutMoreData from "../../Data/FindOutMoreData.json";
-import * as Icons from "../Icons";
+// import * as Icons from "../Icons";
 
 function FindOutMore() {
-  const CONTENT = FindOutMoreData.content;
+  const CONTENT = FindOutMoreData.content,
+    [fadeIn, setFadeIn] = useState(false);
+
+  function handleFadeIn() {
+    setTimeout(() => {
+      setFadeIn(true);
+    }, 2000);
+  }
+
+  useEffect(() => {
+    handleFadeIn();
+  });
 
   return (
     <section className="find-out background--black">
       <div className="container">
-        <h1>{CONTENT[0].title}</h1>
-        <ButtonPrimary 
+        <h1 className={`${"load"} ${fadeIn ? "hasLoaded" : ""}`}>
+          {CONTENT[0].title}
+        </h1>
+        <ButtonPrimary
           link={"#whoWeAre"}
           title={"Find out more"}
+          id={"mainBtn"}
+          classes={`${"load"} ${fadeIn ? "hasLoaded" : ""}`}
         />
       </div>
-      {Icons.Code}
+      {/* {Icons.Code}
       {Icons.Hash}
-      {Icons.Heart}
+      {Icons.Heart} */}
     </section>
-  )
+  );
 }
 
 export default FindOutMore;
