6231802385bfddf4ec8ab2feaa3eccfeadef6317
Luke Charles <lukeowaincharles@gmail.com>
2020-04-03 22:06:31 +0100

    Added id and props classes to button, updated the styling for the transition

--- src/Components/Buttons/ButtonPrimary.js
+++ src/Components/Buttons/ButtonPrimary.js
@@ -5,7 +5,8 @@ function ButtonPrimary(props) {
   return (
     <React.Fragment>
       <Link
-        className="btn btn-primary"
+        className={`${"btn btn-primary"} ${props.classes}`}
+        id={props.id}
         to={props.link}
         href={props.link}
         smooth={true}
