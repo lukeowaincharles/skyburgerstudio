f9b5b160b5531124d780626ac8f24c45ed1ffb18
Luke Charles <lukeowaincharles@gmail.com>
2020-04-03 17:59:02 +0100

    Updated the Icons to be single exportable elements

---
+++ src/Components/Icons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactComponent as CodeIcon } from "../Assets/images/coding.svg";
+import { ReactComponent as HashIcon } from "../Assets/images/hashtag.svg";
+import { ReactComponent as HeartIcon } from "../Assets/images/heart.svg";
+
+export const Code = <CodeIcon className="icon icon--code" />
+
+export const Hash = <HashIcon className="icon icon--hash" />
+
+export const Heart = <HeartIcon className="icon icon--heart" />
