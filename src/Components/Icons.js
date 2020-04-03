6c533a85213e2788a26b289bb0ca7ee9c31433da
Luke Charles <lukeowaincharles@gmail.com>
2020-04-03 20:39:51 +0100

    Added more svgs and updated the Icons file to export them all

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
+import { ReactComponent as CloseIcon } from "../Assets/images/close.svg";
 import { ReactComponent as CodeIcon } from "../Assets/images/coding.svg";
+import { ReactComponent as CursorIcon } from "../Assets/images/cursor.svg";
+import { ReactComponent as DotsIcon } from "../Assets/images/dots.svg";
+import { ReactComponent as ExclaimIcon } from "../Assets/images/exclaim.svg";
 import { ReactComponent as HashIcon } from "../Assets/images/hashtag.svg";
 import { ReactComponent as HeartIcon } from "../Assets/images/heart.svg";
+import { ReactComponent as NoIcon } from "../Assets/images/no.svg";
+import { ReactComponent as OkIcon } from "../Assets/images/ok.svg";
+import { ReactComponent as PencilIcon } from "../Assets/images/pencil.svg";
+import { ReactComponent as PlusIcon } from "../Assets/images/plus.svg";
+import { ReactComponent as SadIcon } from "../Assets/images/sad.svg";
+import { ReactComponent as SearchIcon } from "../Assets/images/search.svg";
+import { ReactComponent as SmilesIcon } from "../Assets/images/smiles.svg";
+import { ReactComponent as YaIcon } from "../Assets/images/ya.svg";
 
-export const Code = <CodeIcon className="icon icon--code" />
-
-export const Hash = <HashIcon className="icon icon--hash" />
-
-export const Heart = <HeartIcon className="icon icon--heart" />
+export const Close = <CloseIcon className="icon icon--close" />;
+export const Code = <CodeIcon className="icon icon--code" />;
+export const Cursor = <CursorIcon className="icon icon--cursor" />;
+export const Dots = <DotsIcon className="icon icon--dots" />;
+export const Exclaim = <ExclaimIcon className="icon icon--exclaim" />;
+export const Hash = <HashIcon className="icon icon--hash" />;
+export const Heart = <HeartIcon className="icon icon--heart" />;
+export const No = <NoIcon className="icon icon--no" />;
+export const Ok = <OkIcon className="icon icon--ok" />;
+export const Pencil = <PencilIcon className="icon icon--pencil" />;
+export const Plus = <PlusIcon className="icon icon--plus" />;
+export const Sad = <SadIcon className="icon icon--sad" />;
+export const Search = <SearchIcon className="icon icon--search" />;
+export const Smiles = <SmilesIcon className="icon icon--smiles" />;
+export const Ya = <YaIcon className="icon icon--ya" />;
