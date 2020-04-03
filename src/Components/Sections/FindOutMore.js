7222a5d79baea68adacbf06ccbcdd70db3988aba
Luke Charles <lukeowaincharles@gmail.com>
2020-04-03 17:59:30 +0100

    Updated the file to bring in the single icons

--- src/Components/Sections/FindOutMore.js
+++ src/Components/Sections/FindOutMore.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ButtonPrimary from '../Buttons/ButtonPrimary';
 import FindOutMoreData from "../../Data/FindOutMoreData.json";
-import { ReactComponent as CodeIcon } from "../../Assets/images/coding.svg";
-import { ReactComponent as HashIcon } from "../../Assets/images/hashtag.svg";
-import { ReactComponent as HeartIcon } from "../../Assets/images/heart.svg";
+import * as Icons from "../Icons";
 
 function FindOutMore() {
   const CONTENT = FindOutMoreData.content;
@@ -17,9 +15,9 @@ function FindOutMore() {
           title={"Find out more"}
         />
       </div>
-      <CodeIcon className="icon icon--code" />
-      <HashIcon className="icon icon--hash" />
-      <HeartIcon className="icon icon--heart" />
+      {Icons.Code}
+      {Icons.Hash}
+      {Icons.Heart}
     </section>
   )
 }
