14cd98de0aa3c5be1edfc0b38463cf7fc75a197d
Luke Charles <lukeowaincharles@gmail.com>
2020-04-18 14:23:18 +0100

    Created initial happy birthday component, updated app.js with the router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.scss';
-import FindOutMore from './Components/Sections/FindOutMore';
-import WhoWeAre from './Components/Sections/WhoWeAre';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.scss";
+import FindOutMore from "./Components/Sections/FindOutMore";
+import WhoWeAre from "./Components/Sections/WhoWeAre";
+import HappyBirthday from "./Components/Sections/HappyBirthday";
 
 function App() {
   return (
     <div className="App">
-      <FindOutMore />
-      <WhoWeAre />
+      <BrowserRouter>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <React.Fragment>
+              <FindOutMore />
+              <WhoWeAre />
+            </React.Fragment>
+          )}
+        />
+        <Route exact path="/happy-birthday" render={() => <HappyBirthday />} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Sections/HappyBirthday.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HappyBirthday() {
+  return (
+    <section className="happy-birthday">
+      <div className="container">
+        <h1>Happy Birthday!!</h1>
+      </div>
+    </section>
+  )
+}
+
+export default HappyBirthday;
