a595f9f63d91f2c75fbdfd87833f9591d036298a
Luke Charles <lukeowaincharles@gmail.com>
2020-09-17 17:34:11 +0100

    Added experience section

---
+++ src/Components/Sections/Experience.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ExperienceData from "../../Data/ExperienceData.json";
+import { Browser } from "../Icons";
+
+function Experience() {
+  const CONTENT = ExperienceData.content;
+
+  return (
+    <section className="experience background--white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div class="browser--icon">
+              {Browser}
+              {Browser}
+            </div>
+          </div>
+          <div className="col-md-5 offset-md-1">
+            <h2>{CONTENT[0].title}</h2>
+            <p>{CONTENT[0].text}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
