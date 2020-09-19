3c7d5d9a6006ea99b0d72084723d152009ffd842
Luke Charles <lukeowaincharles@gmail.com>
2020-09-19 18:32:59 +0100

    Updated about section, added pngs of the 2 of us

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -1,21 +1,48 @@
 import React from "react";
 import AboutData from "../../Data/AboutData.json";
+import { Event } from "../Tracking/index";
 
 function About() {
-  const CONTENT = AboutData.content;
+  const CONTENT = AboutData.content,
+    LINKCONTENT = AboutData.links;
+
+  const links = LINKCONTENT.map((item, index) => {
+    function handleEvent(item) {
+      Event("Find out more", item.title, item.link);
+    }
+
+    return (
+      <p className="font-weight-bold" key={index}>
+        <a
+          href={item.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => {
+            handleEvent(item);
+          }}
+        >
+          {item.text}
+        </a>
+      </p>
+    );
+  });
 
   return (
-    <section className="intro background--white" id="about" name="#about">
+    <section className="about background--white" id="about" name="#about">
       <div className="container">
         <div className="row">
           <div className="col-md-7">
-            <h2>{CONTENT[0].title}</h2>
-            {CONTENT[0].text.map(function (paragraph, i) {
-              return <p key={i}>{paragraph}</p>;
-            })}
+            <h2 dangerouslySetInnerHTML={{ __html: `${CONTENT[0].title}` }} />
+            {CONTENT[0].text.map((paragraph, i) => (
+              <p key={i} dangerouslySetInnerHTML={{ __html: `${paragraph}` }} />
+            ))}
+            {links}
           </div>
-          <div className="col-md-5">
-            <img alt="Placeholder" />
+          <div className="col-md-5 column">
+            <div className="about__images">
+              <img src={LINKCONTENT[1].image} alt={LINKCONTENT[1].alt} />
+              <img src={LINKCONTENT[0].image} alt={LINKCONTENT[0].alt} />
+            </div>
           </div>
         </div>
       </div>
