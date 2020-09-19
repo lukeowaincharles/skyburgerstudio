b3c8b394187f6bc048d6d765db8723b12a99d602
Luke Charles <lukeowaincharles@gmail.com>
2020-09-19 13:01:53 +0100

    Added markup and data for the nav items

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
+import { Link } from "react-scroll";
+import NavData from "../../Data/NavData.json";
 import useWindowWidth from "../Utilities/WindowWidth";
 
 function Navigation() {
   const width = useWindowWidth(),
-    [isOpen, setOpen] = useState(false);
+    [isOpen, setOpen] = useState(false),
+    NAVCONTENT = NavData.content;
 
-  // let delayLink = 0;
-  // if (width <= 1200) {
-  //   delayLink = 800;
-  // }
+  let delayLink = 0;
+  if (width <= 1200) {
+    delayLink = 800;
+  }
+
+  const navItems = NAVCONTENT.map((item, index) => {
+    return (
+      <li key={index}>
+        <Link
+          to={item.link}
+          href={item.link}
+          smooth={true}
+          duration={800}
+          delay={delayLink}
+          offset={-50}
+          className="menu-list--item menu-link"
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+        >
+          {item.title}
+        </Link>
+      </li>
+    );
+  });
 
   return (
     <React.Fragment>
@@ -29,14 +53,14 @@ function Navigation() {
           <div className="overlay">
             {isOpen ? (
               <div className="container">
-
+                <ul>{navItems}</ul>
               </div>
             ) : null}
           </div>
         </div>
       ) : (
         <nav>
-
+          <ul>{navItems}</ul>
         </nav>
       )}
     </React.Fragment>
