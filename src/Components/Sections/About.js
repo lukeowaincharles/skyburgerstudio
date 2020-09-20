53f3d93c054763d6e17eb6c61e6971ceb28c4e70
Luke Charles <lukeowaincharles@gmail.com>
2020-09-20 12:01:19 +0100

    Motion updates to sections of the site

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import AboutData from "../../Data/AboutData.json";
 import { Event } from "../Tracking/index";
+import { IntersectionContext } from "../Utilities/IntersectionObserver";
+import { motion } from "framer-motion";
 
 function About() {
   const CONTENT = AboutData.content,
-    LINKCONTENT = AboutData.links;
+    LINKCONTENT = AboutData.links,
+    { inView } = useContext(IntersectionContext);
+
+  const transition = {
+    duration: 0.8,
+    delay: 0,
+  };
+
+  const variantsLeft = {
+    hidden: {
+      scale: 1,
+      opacity: 0,
+      transition,
+      y: "400px",
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition,
+      y: 0,
+    },
+  };
 
   const links = LINKCONTENT.map((item, index) => {
     function handleEvent(item) {
@@ -30,21 +53,31 @@ function About() {
   return (
     <section className="about background--white" id="about" name="#about">
       <div className="container">
-        <div className="row">
-          <div className="col-md-7">
-            <h2 dangerouslySetInnerHTML={{ __html: `${CONTENT[0].title}` }} />
-            {CONTENT[0].text.map((paragraph, i) => (
-              <p key={i} dangerouslySetInnerHTML={{ __html: `${paragraph}` }} />
-            ))}
-            {links}
-          </div>
-          <div className="col-md-5 column">
-            <div className="about__images">
-              <img src={LINKCONTENT[1].image} alt={LINKCONTENT[1].alt} />
-              <img src={LINKCONTENT[0].image} alt={LINKCONTENT[0].alt} />
+        <motion.div
+          initial="hidden"
+          animate={inView ? "show" : "hidden"}
+          exit="hidden"
+          variants={variantsLeft}
+        >
+          <div className="row">
+            <div className="col-md-7">
+              <h2 dangerouslySetInnerHTML={{ __html: `${CONTENT[0].title}` }} />
+              {CONTENT[0].text.map((paragraph, i) => (
+                <p
+                  key={i}
+                  dangerouslySetInnerHTML={{ __html: `${paragraph}` }}
+                />
+              ))}
+              {links}
+            </div>
+            <div className="col-md-5 column">
+              <div className="about__images">
+                <img src={LINKCONTENT[1].image} alt={LINKCONTENT[1].alt} />
+                <img src={LINKCONTENT[0].image} alt={LINKCONTENT[0].alt} />
+              </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
