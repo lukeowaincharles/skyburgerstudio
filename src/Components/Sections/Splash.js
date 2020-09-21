34caefa668c557ab7d8a0372e7c9b9a78fc91c0b
Luke Charles <lukeowaincharles@gmail.com>
2020-09-21 19:32:30 +0100

    Updated the markup for the splash section - moved the browser background so not part of the main browser

--- src/Components/Sections/Splash.js
+++ src/Components/Sections/Splash.js
@@ -2,27 +2,32 @@ import React from "react";
 import * as Icons from "../Icons";
 
 function Splash() {
-
   return (
     <section className="splash background--black" id="splashPage">
       <div className="container">
-        <div className="browser">
-          <div className="browser__bar">
-            <span></span>
-            <span></span>
-            <span></span>
+        <div className="browser__wrapper">
+          <div className="browser">
+            <div className="browser__bar">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className="browser__window text-center">
+              <p>Branding</p>
+              <h1 className="h1--large letter__space">Skyburger Studio</h1>
+              <p>Web design</p>
+            </div>
           </div>
-          <div className="browser__window text-center">
-            <p>Branding</p>
-            <h1 className="h1--large letter__space">Skyburger Studio</h1>
-            <p>Web design</p>
+          <div className="browser--background">
+            <div className="browser__bar">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
-          <div className="browser--background"></div>
         </div>
       </div>
-      <div
-        className="splash__icons"
-      >
+      <div className="splash__icons">
         <div className="icons__wrapper">
           {Icons.Code}
 
