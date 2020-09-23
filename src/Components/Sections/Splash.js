024a97f1192f528484b605123bab681374ef81c9
Luke Charles <lukeowaincharles@gmail.com>
2020-09-23 19:11:59 +0100

    Updated the markup so the onClick function only happens above 992px

--- src/Components/Sections/Splash.js
+++ src/Components/Sections/Splash.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import * as Icons from "../Icons";
+import useWindowWidth from "../Utilities/WindowWidth";
 
 function Splash() {
-  const [isClosed, setClosed] = useState(false);
+  const [isClosed, setClosed] = useState(false),
+    width = useWindowWidth();
 
   const handleClose = () => {
     setClosed(!isClosed);
@@ -13,21 +15,29 @@ function Splash() {
       <div className="container">
         <div className={`browser__wrapper ${isClosed ? "isClosed" : ""}`}>
           <div className="browser">
-            <div className="browser__bar">
-              <span
-                className="browser__ellipse browser__ellipse--close"
-                onClick={() => {
-                  handleClose();
-                }}
-              ></span>
-              <span
-                className="browser__ellipse browser__ellipse--minimise"
-                onClick={() => {
-                  handleClose();
-                }}
-              ></span>
-              <span className="browser__ellipse browser__ellipse--full-screen"></span>
-            </div>
+            {width >= 992 ? (
+              <div className="browser__bar">
+                <span
+                  className="browser__ellipse browser__ellipse--close"
+                  onClick={() => {
+                    handleClose();
+                  }}
+                ></span>
+                <span
+                  className="browser__ellipse browser__ellipse--minimise"
+                  onClick={() => {
+                    handleClose();
+                  }}
+                ></span>
+                <span className="browser__ellipse browser__ellipse--full-screen"></span>
+              </div>
+            ) : (
+              <div className="browser__bar">
+                <span className="browser__ellipse browser__ellipse--close"></span>
+                <span className="browser__ellipse browser__ellipse--minimise"></span>
+                <span className="browser__ellipse browser__ellipse--full-screen"></span>
+              </div>
+            )}
             <div className="browser__window text-center">
               <p>Branding</p>
               <h1 className="h1--large letter__space">Skyburger Studio</h1>
