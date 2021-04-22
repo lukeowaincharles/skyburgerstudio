efe9b0af84db4fd156a8a19db2b8f374fa466aad
Luke Charles <lukeowaincharles@gmail.com>
2021-04-22 17:44:26 +0100

    Commented out imports and components - bad practice - will remove when re-design is done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.scss";
 import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
-import About from "./Components/Sections/About";
-import Contact from "./Components/Sections/Contact";
-import Experience from "./Components/Sections/Experience";
+// import Header from "./Components/Header/Header";
+// import About from "./Components/Sections/About";
+// import Contact from "./Components/Sections/Contact";
+// import Experience from "./Components/Sections/Experience";
 // import FindOutMore from "./Components/Sections/FindOutMore";
 // import WhoWeAre from "./Components/Sections/WhoWeAre";
 import HappyBirthday from "./Components/Sections/HappyBirthday";
-import Intro from "./Components/Sections/Intro";
-import RecentWork from "./Components/Sections/RecentWork";
+// import Intro from "./Components/Sections/Intro";
+// import RecentWork from "./Components/Sections/RecentWork";
 import Splash from "./Components/Sections/Splash";
-import Testimonials from "./Components/Sections/Testimonials";
-import { IntersectionObserver } from "./Components/Utilities/IntersectionObserver";
+// import Testimonials from "./Components/Sections/Testimonials";
+// import { IntersectionObserver } from "./Components/Utilities/IntersectionObserver";
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
           render={() => (
             <React.Fragment>
               <Splash />
-              <Header />
+              {/* <Header />
               <IntersectionObserver>
                 <Intro />
               </IntersectionObserver>
@@ -35,13 +35,13 @@ function App() {
               <IntersectionObserver>
                 <Experience />
               </IntersectionObserver>
-              <Testimonials />
+              <Testimonials /> */}
               {/* <FindOutMore />
               <WhoWeAre /> */}
-              <IntersectionObserver>
+              {/* <IntersectionObserver>
                 <About />
-              </IntersectionObserver>
-              <Contact />
+              </IntersectionObserver> */}
+              {/* <Contact /> */}
               <Footer />
             </React.Fragment>
           )}
